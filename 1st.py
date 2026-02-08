df1a79a3dc20059dcb33b8ab2862318ee4858187
ahmadaaaz <109924199+ahmadaaaz@users.noreply.github.com>
2026-02-09 01:31:10 +0300

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+
+st.title(" Yerde Emisyon Azaltımı için APU–GPU/PCA Optimizasyon Platformu.")
+
+upload_file = st.file_uploader("Choose a File", type="csv")
+
+if upload_file is not None:
+    df = pd.read_csv(upload_file)
+    st.subheader("Data Preview")
+    st.write(df.head())
+
+    #st.subheader("summery")
+   # st.write(df.describe())
+
+    st.subheader("Filter Data")
+    columns = df.columns.tolist()
+    selected_columns = st.selectbox("Select Columns", columns)
+    unique_values = df[selected_columns].unique()
+    selected_value = st.selectbox("Select Value", unique_values)
+
+    filtered_df = df[df[selected_columns] == selected_value]
+    st.write(filtered_df)
+
+
+    st.subheader("Plot Data")
+    x_column = st.selectbox("Select X Column", columns)
+    y_column = st.selectbox("Select Y Column", columns)
+
+    if st.button("display chart"):
+        st.line_chart(filtered_df.set_index(x_column)[y_column])
+else:
+    st.write("waiting for file upload . . .")
